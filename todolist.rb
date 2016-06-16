7bac858fefb837b4e2dba30dd2f506a63b92fdf0
Enrique Barragan <ebarragan1997@gmail.com>
2016-06-16 17:58:43 -0500

    Initial commit, finished TodoList and Item class.

---
+++ todolist.rb
@@ -0,0 +1,24 @@
+class TodoList
+  attr_reader :title, :items
+
+  def initialize(list_title)
+      @title = list_title
+      @items = Array.new
+  end
+
+  def add_item(new_item)
+    item = Item.new(new_item)
+    @items.push(item)
+  end
+
+end
+
+class Item
+  attr_reader :description, :completed_status
+
+   def initialize(item_description)
+      @description = item_description
+      @completed_status = false
+   end
+
+end
